dc6255e1487c8d6ff874014a5e4a9c7b1779c184
xyttyx <yt.xiong@qq.com>
2025-04-23 11:54:22 +0800

    添加对数据集的测试

---
+++ test/test_dataset.py
@@ -0,0 +1,16 @@
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from utils import AppDataset
+
+# 测试数据集使用“直接输出上一个应用作为预测值”策略的accuracy
+# 准确率为 60.13%
+dataset = AppDataset(DatasetPath="Dataset", length=2)
+
+count = 0
+for i in range(len(dataset.App_usage_trace)):
+    if dataset.App_usage_trace[i][0][0, -1] == dataset.App_usage_trace[i][0][1, -1]:
+        count += 1
+
+print(count / len(dataset.App_usage_trace))
