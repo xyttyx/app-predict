3f56e22caf8d46a1f86f126522e1db10a766a195
xyttyx <yt.xiong@qq.com>
2025-04-26 21:36:05 +0800

    优化metrics函数在k=1时的准确率计算逻辑

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,7 +1,7 @@
 import os
 import torch
 import numpy as np
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, top_k_accuracy_score
+from sklearn.metrics import accuracy_score, top_k_accuracy_score
 from typing import Dict
 
 def metrics(
@@ -12,7 +12,11 @@ def metrics(
 ) -> Dict[str, float]:
     acc = []
     for i in range(1,k+1):
-        accuracy = top_k_accuracy_score(y_true, y_pred, k=i, labels=np.arange(0, n_classes))
+        if k==1:
+            pred = np.argmax(y_pred,axis=1)
+            accuracy = accuracy_score(y_true, pred)
+        else:
+            accuracy = top_k_accuracy_score(y_true, y_pred, k=i, labels=np.arange(0, n_classes))
         acc.append([i, accuracy])
     acc = dict(acc)
 
