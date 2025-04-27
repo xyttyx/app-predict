3d96283d582f5f2ce05b85ad1f0da2b4feb0225b
xyttyx <yt.xiong@qq.com>
2025-04-27 14:44:45 +0800

    常规测试修改

---
+++ trainAppEmbedding.py
@@ -0,0 +1,53 @@
+import os
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+from Models import DeepWalkWeighted
+from utils import AppDataset
+
+import pickle
+
+if os.path.exists("./Dataset/word2vec_data.pt"):
+    word2vec_data = torch.load("./Dataset/word2vec_data.pt")
+else:
+    data = AppDataset(length = 7)
+    word2vec_data = []
+    for i in range(len(data)):
+        tmp = data[i][0]
+        apps = tmp[:,4]
+        app = apps[3]
+        for j in range(len(apps)):
+            app2 = apps[j]
+            if app2 != app:
+                word2vec_data.append(torch.tensor([app, app2],dtype=torch.int16))
+    word2vec_data = torch.stack(word2vec_data)
+    torch.save(word2vec_data, "./Dataset/word2vec_data.pt")
+
+HeteroGraph = pickle.load(open("./Dataset/graph_data.pkl", "rb"))
+edge_index_app = HeteroGraph['app', 'to', 'app'].edge_index
+edge_weight_app = HeteroGraph['app', 'to', 'app'].attr
+app_num = 1696
+device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
+
+pretrained_weight = torch.load("./Dataset/app_embeddings.pt", weights_only=False)
+
+model = DeepWalkWeighted(
+    num_nodes=app_num,
+    edge_index=edge_index_app,
+    edge_weight=edge_weight_app,
+    embedding_size=200,
+    pretrained_weight = pretrained_weight,
+    walk_length=5,
+    num_walks=30,
+    batch_size=256,
+    lr = 1e-4,
+    epochs=20,
+    sampling_times=20,
+    device=device
+)
+
+embeddings = model.train()
+torch.save(embeddings, "./Dataset/app_embeddings.pt")
